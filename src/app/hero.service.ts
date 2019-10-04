ccd642eb771c53dda8c67437a02684dc921dc24f
joshmakar <joshmakar@gmail.com>
2019-10-04 16:02:21 -0400

    Complete Routing Section

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -19,4 +19,10 @@ export class HeroService {
     this.messageService.add('HeroService: Fetched Heroes.');
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: Fetched with id: ${id}`);
+
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
