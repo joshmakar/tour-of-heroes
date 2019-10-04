772d3d344963ae855983d7da308af62b9e8d32bf
joshmakar <joshmakar@gmail.com>
2019-10-04 17:54:42 -0400

    Fixed revert conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,26 @@
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { HttpClientModule } from '@angular/common/http';
-import { InMemoryDataService } from './in-memory-data.service';
-import { MessagesComponent } from './messages/messages.component';
-<<<<<<< HEAD
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-=======
->>>>>>> parent of ccd642e... Complete Routing Section
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     FormsModule,
-    AppRoutingModule,
-    HttpClientModule
-=======
-    FormsModule
->>>>>>> parent of ccd642e... Complete Routing Section
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
