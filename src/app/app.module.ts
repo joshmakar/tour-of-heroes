456e2bd6521277095313c82c37979a92a0da6285
joshmakar <joshmakar@gmail.com>
2019-10-04 17:05:03 -0400

    About to revert back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDataService } from './in-memory-data.service';
+import { MessagesComponent } from './messages/messages.component';
+import { NgModule } from '@angular/core';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +23,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,8 @@
+import { Hero } from './hero';
 import { Injectable } from '@angular/core';
 import { Observable, of} from 'rxjs';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 import { MessageService } from './message.service';
 
@@ -10,19 +10,25 @@ import { MessageService } from './message.service';
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(
+    private http: HttpClient,
     private messageService: MessageService
   ) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: Fetched Heroes.');
-    return of(HEROES);
+    this.log('HeroService: Fetched Heroes.');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: Fetched with id: ${id}`);
-
+    this.log(`HeroService: Fetched with id: ${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
