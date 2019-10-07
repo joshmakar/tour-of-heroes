4e7e9a8ee9dd0ef5666d66110928ec54b81b02d2
joshmakar <joshmakar@gmail.com>
2019-10-07 15:10:30 -0400

    Finish HTTP section

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,31 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
-import { HeroService } from '../hero.service';
 import { Location } from '@angular/common';
+
+import { Hero }         from '../hero';
+import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+  styleUrls: [ './hero-detail.component.scss' ]
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero;
-  
+  @Input() hero: Hero;
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) { }
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getHero();
   }
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
@@ -34,4 +33,8 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+ save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
